19b2ea27840affac13beb39e4ad40abe8dd9bfb9
Rodriguez <rodriguez@gmail.com>
2026-05-01 11:06:19 -0500

    agrego rutas publicas

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,3 +38,27 @@ resource "aws_subnet" "public_b" {
     Name = "public-b-${var.env}"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt-${var.env}"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}

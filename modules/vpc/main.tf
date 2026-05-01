a9b4fab1278abbfc7ba4221304eeeba7f4d765da
Rodriguez <rodriguez@gmail.com>
2026-05-01 11:04:17 -0500

    agrego subnets publicas

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,25 @@ resource "aws_internet_gateway" "igw" {
     Name = "igw-${var.env}"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-a-${var.env}"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-b-${var.env}"
+  }
+}

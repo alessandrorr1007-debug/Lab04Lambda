bc87ca210f923dd667490d6d83e425d635480a59
Rodriguez <rodriguez@gmail.com>
2026-05-01 11:07:30 -0500

    agrego subnets privadas

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,3 +62,23 @@ resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-a-${var.env}"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-b-${var.env}"
+  }
+}

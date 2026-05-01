04ee0a3bc689f0c3b68dc5479cd908a5c82248ba
Rodriguez <rodriguez@gmail.com>
2026-05-01 11:11:44 -0500

    agrego rutas privadas con nat

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -108,3 +108,41 @@ resource "aws_nat_gateway" "nat_b" {
     Name = "nat-b-${var.env}"
   }
 }
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-rt-a-${var.env}"
+  }
+}
+
+resource "aws_route" "private_a_nat" {
+  route_table_id         = aws_route_table.private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_a.id
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-rt-b-${var.env}"
+  }
+}
+
+resource "aws_route" "private_b_nat" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_b.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
